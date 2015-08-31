1b75a2873fc0f405314c046249a6303af19c0a94
Sarah Rogers <email": null>
2015-08-31 17:24:27 +1200

    Add persistent links on footer for Privacy and Terms of Use

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,8 +1,21 @@
 class SiteController < ApplicationController
+
+before_filter :set_title
+
   def index
-    @title = "Produce App"
   end
 
   def about
   end
+
+  def privacy
+  end
+
+  def terms
+  end
+
+  private
+    def set_title
+      @title = "Co-op App"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'site#index'
 
   get '/about' => 'site#about'
+  get '/privacy' => 'site#privacy'
+  get '/terms' => 'site#terms'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
